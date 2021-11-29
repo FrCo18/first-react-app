b85ee61e6b368d6d06030e693b0879645f455f74
Nikita <nikita2015borisov@gmail.com>
2021-11-29 23:04:53 +0300

    my input add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -13,12 +14,26 @@ function App() {
         {id: 3, title: 'JavaScript', body: 'Description'},
     ])
 
+    const [title, setTitle] = useState('');
+    const bodyInputRef = useRef();
+
+    function addNewPost(e){
+        e.preventDefault()
+        console.log(title)
+        console.log(bodyInputRef.current.valueOf().value)
+    }
+
     return (
         <div className="App">
             <form>
-                <input type="text" placeholder="Название поста"/>
-                <input type="text" placeholder="Описсание текста"/>
-                <MyButton>Создать пост</MyButton>
+                <MyInput
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                    type="text" placeholder="Название поста"/>
+                <MyInput
+                    ref={bodyInputRef}
+                    type="text" placeholder="Описсание текста"/>
+                <MyButton onClick={addNewPost}>Создать пост</MyButton>
             </form>
             <PostList posts={posts} title="Посты про JS"/>
         </div>
